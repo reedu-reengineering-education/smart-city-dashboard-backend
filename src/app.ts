37109692ec8ec01589d42ca4c132de5743861269
felixerdy <felix.erdmann@uni-muenster.de>
2020-12-08 19:45:12 +0100

    make beautiful code

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,33 @@
-import express from "express";
-import asyncRedis from "async-redis";
-import axios from "axios";
+import express from 'express';
+import asyncRedis from 'async-redis';
+import axios from 'axios';
 
 const port: number = 3000;
 
 const app = express();
 
 const isProduction =
-  process.env.NODE_ENV !== undefined && process.env.NODE_ENV === "production";
+  process.env.NODE_ENV !== undefined && process.env.NODE_ENV === 'production';
 
 const client = asyncRedis.createClient({
-  host: isProduction ? "redis" : "127.0.0.1",
+  host: isProduction ? 'redis' : '127.0.0.1',
 });
 
-client.on("error", (error) => {
+client.on('error', (error) => {
   console.error(error);
 });
 
-client.on("connect", async () => {
+client.on('connect', async () => {
   const parkhausReq = await axios.get(
-    "https://www.stadt-muenster.de/index.php?id=10910"
+    'https://www.stadt-muenster.de/index.php?id=10910'
   );
   const parkhausData = await parkhausReq.data;
-  await client.set("parkhaus", JSON.stringify(parkhausData));
+  await client.set('parkhaus', JSON.stringify(parkhausData));
 });
 
-app.get("/", async (req, res) => {
-  const data = await client.get("parkhaus");
-  res.setHeader("Content-Type", "application/json");
+app.get('/', async (req, res) => {
+  const data = await client.get('parkhaus');
+  res.setHeader('Content-Type', 'application/json');
   res.send(data);
 });
 
